faca5d48c01d9e80a51ac49a37615d7beb37cdf4
Jack Liu <jack980517@gmail.com>
2014-12-04 12:35:37 +0800

    First commit

---
+++ extractor.py
@@ -0,0 +1,11 @@
+list=open('00.txt','r')
+for i in range(0,211):
+    dat=list.readline()[:-1]
+    m4a='M4A/'+dat[4:-3]+'m4a'
+    datfile=open(dat,'r')
+    m4afile=open(m4a,'w')
+    s=datfile.read()
+    a=s.find('ftyp')
+    a-=4
+    b=s.find(chr(0x1)+chr(0x39)+chr(0)+chr(0x10)+chr(0x27)+chr(0)+chr(0)+chr(0x70)+chr(0x17))
+    m4afile.write(s[a:b])
---
+++ info.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+genre=["J-POP","アニメ","バラエティ","クラシック","ナムコオリジナル","ダウンロード","ゲームミュージック","童謠•民謠"]
+list=open('00.txt','r')
+result=open('result.txt','w')
+result.write('Filename\tGenre\tTitle\n')
+for i in range(0,211):
+    dat=list.readline()[:-1]
+    datfile=open(dat,'r')
+    s=datfile.read()
+    a=s.find('ftyp')-5
+    g=ord(s[4])
+    result.write(dat[4:]+'\t'+genre[g]+'\t'+s[80:a]+'\n')
